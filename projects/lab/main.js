79fc77c67c51e931e7537367fb4b9bf4aeb01d1c
iqban943 <iqban943@newschool.edu>
2017-03-29 23:14:11 -0400

    Adding JS exercise

---
+++ projects/lab/main.js
@@ -0,0 +1,32 @@
+var springBreak = '9.5 hours of sleep per night';
+
+6*11;
+
+22*3;
+
+33*2;
+
+66*1;
+
+66/1;
+
+6*(9+2);
+
+var groupOne = ['Carol', 'Kyle', 'Nafisa'];
+var groupTwo = ['Vitoria', 'Liz', 'Jissaura'];
+var groupThree = ['Ana Sofia', 'Gaurika', 'Moon']
+
+var allStudentsHere = false;
+var studentsMissing = true;
+
+var allGroups= [groupOne, groupTwo, groupThree]
+
+for (var index1 = 0; index1 < allGroups.length; index1++ ) {
+  console.log(' This group is number ' + (index1 +1 ));
+  console.log (' The sudents in this group are:');
+
+for (var index2 = 0; index2 < allGroups[index1].length;index2++){
+  console.log( allGroups[index1][index2] );
+  }
+}
+
