9730d99ce775b6a89b0a4fe3abd3ab98243322c4
iqban943 <iqban943@newschool.edu>
2017-04-05 19:47:21 -0400

    Ughhh test

---
+++ projects/lab_test/main.js
@@ -0,0 +1,48 @@
+'Nafisa Afrin Iqbal';
+1;
+'1';
+PokemonIsCool = true;
+var myName = ['N', 'A', 'F', 'I', 'S', 'A'];
+['O', 'K', 1, 2, 3, true, false];
+'Nafisa' + 'Iqbal';
+70 + 30;
+
+var firstItem = myName [0];
+var lastItem = myName [myName.length - 1];
+
+// Missed number 3
+var firstName = 'Nafisa';
+var lastName = 'Iqbal';
+firstName.length == lastName.length;
+
+var allStudentsHere = true;
+
+if (allStudentsHere = true) {
+  console.log("All students are in class");
+} else if (allStudentsHere = false) {
+  console.log("Not everyone is here");
+}
+
+for (var i = 0; i < myName.length; i++) {
+  console.log (myName[i] + 'is in index' + i);
+}
+
+var date = new Date;
+var hour = date.getHours ();
+
+if (hour > 7 && hour < 940) {
+  console.log("It's class time!");
+} else {
+  console.log("Not class time");
+}
+
+// skipped 8
+
+var favoriteFoods = ['smoothie', 'bubble tea', 'biryani'];
+var favoriteAnimals = ['cat', 'owl', 'deer'];
+var favoritePlaces = ['Florence', 'Dhaka', 'New York'];
+var favoriteThreePokemon = ['Blaziken', 'Snorlax', 'Magikarp'];
+
+var arrayOfArrays = [favoriteFoods, favoriteAnimals, favoritePlaces, favoritePokemon];
+
+
