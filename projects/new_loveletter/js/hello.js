27e9ae46ee7e6ade3036217231d7659d5e58c35d
iqban943 <iqban943@newschool.edu>
2017-04-18 08:02:34 -0400

    Final

--- projects/new_loveletter/js/hello.js
+++ projects/new_loveletter/js/hello.js
@@ -1,8 +1,14 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-        document.getElementById("myP").className = "test2";
+    if (document.body.scrollTop > 2500 || document.documentElement.scrollTop > 2500) {
+        document.getElementById("myP").className = "test5";
+    } else if (document.body.scrollTop > 1900 || document.documentElement.scrollTop > 1900) {
+                document.getElementById("myP").className = "test4";
+    } else if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+                document.getElementById("myP").className = "test3";
+    } else if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+                document.getElementById("myP").className = "test2";
     } else {
       document.getElementById("myP").className = "test";
     }
